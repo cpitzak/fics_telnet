a4a83796412e3e77c90ef52c68599da794be47d6
Clint Pitzak <cpitzak@gmail.com>
2019-06-14 17:33:16 -0700

    Updates to fics telnet

--- socketServer.js
+++ socketServer.js
@@ -5,7 +5,8 @@ const wss = new WebSocket.Server({ port: 3000 });
 wss.on('connection', function connection(ws) {
 
     var Telnet = require('telnet-client');
-    var connection = new Telnet();
+    var telnet = new Telnet();
+    var cmd = 'ping';
 
     var params = {
         host: 'freechess.org',
@@ -13,60 +14,51 @@ wss.on('connection', function connection(ws) {
         shellPrompt: /fics[^%]*\%\s*$/,
         loginPrompt: 'login: ',
         passwordPrompt: 'password: ',
-        username: 'nivo',
-        password: 'osdcpi',
         initialLFCR: true,
         timeout: 1000 * 60 * 15,
         failedLoginMatch : "Login incorrect",
       // removeEcho: 4
-    }
-
-    var cmd = 'ping';
+    };
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
         let command = message.replace(/\"/g, '');
 
-        connection.send(command, function(err, res2) {
+        telnet.send(command, function(err, res2) {
             if (err) {
                 console.log('============== ERROR =============');
             }
-            ws.send(res2);
         })
 
       });
 
-    connection.on('ready', function(prompt) {
+    telnet.on('ready', function(prompt) {
         var mins = 1000 * 60 * 14;
-        console.log(prompt);
-        console.log('ready');
         setInterval(function() {
-            console.log(">" + cmd);
-            connection.exec("ping", function(err, response) {
+            telnet.exec("ping", function(err, response) {
                 console.log("<",response)
             });
         }, mins);
-    })
+    });
 
-    connection.on('data',function(msg) {
-        // console.log("stuff: ", msg.toString('utf8'));
+    telnet.on('data',function(msg) {
         ws.send(msg.toString('utf8'));
     });
 
 
-    connection.on('failedlogin',function(msg) {
+    telnet.on('failedlogin',function(msg) {
         console.log("Login failed !",msg);
     });
 
-    connection.on('timeout', function() {
+    telnet.on('timeout', function() {
         console.log('socket timeout!')
-        connection.end()
-    })
+        telnet.end()
+    });
 
-    connection.on('close', function() {
+    telnet.on('close', function() {
         console.log('connection closed')
-    })
+    });
 
-    connection.connect(params)
-  
+    telnet.connect(params);
+    ws.send('xyz134connected');
 });
